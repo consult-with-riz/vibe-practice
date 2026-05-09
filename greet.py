6e13ac7021d93410e923d376d6b58f5d7e221afd
Riz from Soch <riz@withsoch.com>
2026-05-09 20:23:31 +0300

    Updated README with project idea

---
+++ greet.py
@@ -0,0 +1,6 @@
+def greet(name):
+    return "Hello, " + name + "!"
+ 
+print(greet("Husnain"))
+print(greet("Mahad"))
+print(greet("Bilal"))
---
+++ variables.py
@@ -0,0 +1,9 @@
+name = "Ahmad"
+age = 28
+city = "Lahore"
+is_employed = True
+ 
+print(name)
+print(age)
+print(city)
+print(is_employed)
